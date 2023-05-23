728c84d6647926de772fd713225e5f4f089b426c
Trollicus <40140975+Trollicus@users.noreply.github.com>
2023-05-23 14:47:22 +0300

    X-Captcha-Key
    they fr tried.

--- SupremeSystem/Program.cs
+++ SupremeSystem/Program.cs
@@ -1,5 +1,5 @@
 ﻿var response = await PostAsync("https://discord.com/api/v9/auth/register", HttpMethod.Post,
-    $"{{\"fingerprint\": \"{await FingerPrint()}\", \"email\": \"{await CreateMail()}\", \"password\": \"{RandomString(13)}\", \"username\": \"UnrealContent\", \"consent\": true, \"date_of_birth\": \"1999-12-07\", \"captcha_key\": \"{await RunCaptcha(CaptchaType.AntiCaptcha)}\", \"invite\": \"pXUUXxZk\", \"gift_code_sku_id\":null, \"promotional_email_opt_in\": false}}",
+    $"{{\"fingerprint\": \"{await FingerPrint()}\", \"email\": \"{await CreateMail()}\", \"password\": \"{RandomString(13)}\", \"username\": \"UnrealContent\", \"consent\": true, \"date_of_birth\": \"1999-12-07\", \"captcha_key\": null, \"invite\": \"pXUUXxZk\", \"gift_code_sku_id\":null, \"promotional_email_opt_in\": false}}",
     new[]
     {
         new RequestHeadersEx("X-Fingerprint", await FingerPrint()),
@@ -10,6 +10,7 @@
         new RequestHeadersEx("User-Agent", UserAgent),
         new RequestHeadersEx("Host", "discord.com"),
         new RequestHeadersEx("X-Discord-Locale", "en-US"),
+        new RequestHeadersEx("X-Captcha-Key", await RunCaptcha(CaptchaType.AntiCaptcha))
     }); 
 
 Console.WriteLine(await response.Content.ReadAsStringAsync());
